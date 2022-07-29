eaffefe10722df7ff91988eb59f5f686f23b2a3e
Linkweb_Tech <technique@linkweb.fr>
2022-07-29 16:10:14 +0200

    Add no-index in head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,6 +24,7 @@ class CustomDocument extends Document {
           />
           <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
           <link rel='shortcut icon' href='/images/favicon.png' />
+          <meta name="robots" content="noindex,nofollow"/>
         </Head>
         <body>
           <Main />
